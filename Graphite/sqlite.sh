b13d0a142a42181377099689d2db1283f4d13fa4
Greg Wolf <gwolf@akamai.com>
2023-10-09 11:06:16 -0400

    Reverted back to /usr/local/graphite

--- Graphite/sqlite.sh
+++ Graphite/sqlite.sh
@@ -3,15 +3,11 @@
 ############################################
 #                                          #
 #              sqlite.sh                   #
-#                  v 3                     #
+#                  v 4                     #
 #                                          #
 ############################################
 
 # The purpose of this script is to reduce the size of the graphite.db by removing old tags
 # This script is triggered remotely but executed directly on the Graphite Linode
 
-# This version uses a root of /mnt/data/ for Graphite, which in turn points to an external volume
-# The existing Stackscript installs Graphite using a root path of /usr/local/graphite/
-# If an external volume is used, be sure to create the volume and then update the script to use the correct root
-
-sqlite3 /mnt/data/graphite-storage/graphite.db < /usr/local/graphite/deleteoldevents.sql && sqlite3 /mnt/data/graphite-storage/graphite.db 'VACUUM;'
+sqlite3 /usr/local/graphite/graphite-storage/graphite.db < /usr/local/graphite/deleteoldevents.sql && sqlite3 /usr/local/graphite/graphite-storage/graphite.db 'VACUUM;'
